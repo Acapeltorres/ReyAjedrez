e09d76c2b8288d7dd475d46bbaea44bb3bc3c64a
Acapeltorres <luchocapel@icloud.com>
2023-11-14 20:01:45 +0100

    Añado constructor con parametros

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Posicion.java
@@ -5,7 +5,16 @@ public class Posicion {
     private int fila;
     private char columna;
 
+    //Constructor con parametros (llamada a los setters)
 
+    public Posicion(int fila, char columna){
+
+        setFila(fila);
+        setColumna(columna);
+
+    }
+
+    // getters y setters
 
     public int getFila() {
         return fila;
@@ -13,7 +22,7 @@ public class Posicion {
 
     public void setFila(int fila) {
         if (fila<1 || fila>8){
-            throw new IllegalArgumentException("ERROR: Fila no valida");
+            throw new IllegalArgumentException("ERROR: Fila no valida (inserte fila entre la '1' y la '8')");
         }else{
             this.fila = fila;
         }
@@ -26,7 +35,7 @@ public class Posicion {
 
     public void setColumna(char columna) {
         if(columna< 'a' || columna> 'h'){
-            throw new IllegalArgumentException("ERROR: Fila no valida");
+            throw new IllegalArgumentException("ERROR: columna no valida (inserte columna entre la 'a' y la 'h')");
         }else{
             this.columna = columna;
         }
