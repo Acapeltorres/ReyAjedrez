1949df488ebe674a28efb9fe5fef8a02208bf4e1
Acapeltorres <luchocapel@icloud.com>
2023-11-14 21:40:05 +0100

    Añado getters y setter con las validaciones pertinentes

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
@@ -7,4 +7,46 @@ public class Rey {
     private Color color;
     private Posicion posicion;
     private int totalMovimientos;
+
+    //getters y setters
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        if(color==null){
+            throw new NullPointerException("ERROR: Color incorrecto");
+        }else{
+            this.color = color;
+        }
+
+    }
+
+    public Posicion getPosicion() {
+        return posicion;
+
+    }
+
+    public void setPosicion(Posicion posicion) {
+
+        if(posicion==null){
+            throw new NullPointerException("ERROR: Posicion erronea");
+        }else {
+            this.posicion = posicion;
+        }
+    }
+
+    public int getTotalMovimientos() {
+        return totalMovimientos;
+    }
+
+    public void setTotalMovimientos(int totalMovimientos) {
+        if(totalMovimientos<=0){
+            throw new IllegalArgumentException("ERROR: total movimientos incorrecto");
+        }else{
+            this.totalMovimientos = totalMovimientos;
+        }
+
+    }
 }
