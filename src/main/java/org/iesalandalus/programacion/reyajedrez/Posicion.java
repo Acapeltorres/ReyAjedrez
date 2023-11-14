17138990c40a8551ba2b1db9e839533653d27114
Acapeltorres <luchocapel@icloud.com>
2023-11-14 19:46:53 +0100

    Añado metodos getters y setters con sus restricciones correspondientes

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Posicion.java
@@ -4,4 +4,32 @@ public class Posicion {
     //Atributos
     private int fila;
     private char columna;
+
+
+
+    public int getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila) {
+        if (fila<1 || fila>8){
+            throw new IllegalArgumentException("ERROR: Fila no valida");
+        }else{
+            this.fila = fila;
+        }
+
+    }
+
+    public char getColumna() {
+        return columna;
+    }
+
+    public void setColumna(char columna) {
+        if(columna< 'a' || columna> 'h'){
+            throw new IllegalArgumentException("ERROR: Fila no valida");
+        }else{
+            this.columna = columna;
+        }
+
+    }
 }
