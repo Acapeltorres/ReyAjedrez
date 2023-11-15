85fad0eee40113d6567faa2ede86d4d4cd394203
Acapeltorres <luchocapel@icloud.com>
2023-11-15 19:50:29 +0100

    Añado metodo mover y toStrin a la clase rey

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
@@ -3,6 +3,8 @@ package org.iesalandalus.programacion.reyajedrez.modelo;
 import org.iesalandalus.programacion.reyajedrez.modelo.Color;
 import org.iesalandalus.programacion.reyajedrez.modelo.Posicion;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Rey {
     private Color color;
     private Posicion posicion;
@@ -32,7 +34,7 @@ public class Rey {
         return color;
     }
 
-    public void setColor(Color color) {
+    private void setColor(Color color) {
         if(color==null){
             throw new NullPointerException("ERROR: Color incorrecto");
         }else{
@@ -46,7 +48,7 @@ public class Rey {
 
     }
 
-    public void setPosicion(Posicion posicion) {
+    private void setPosicion(Posicion posicion) {
 
         if(posicion==null){
             throw new NullPointerException("ERROR: Posicion erronea");
@@ -66,5 +68,97 @@ public class Rey {
             this.totalMovimientos = totalMovimientos;
         }
 
+    }
+
+    public void mover(Direccion d) throws OperationNotSupportedException,IllegalArgumentException{
+        int totalMovimientos=0;
+        if(d==null){
+            throw new NullPointerException("ERROR: direcion incorrecta");
+        }
+        if(d.equals(Direccion.SUR)){
+            try{
+                setPosicion(new Posicion(posicion.getFila()-1,posicion.getColumna()));
+                totalMovimientos++;
+            }catch (IllegalArgumentException e) {
+                throw new OperationNotSupportedException("ERROR");
+            }
+        } else if (d.equals(Direccion.NORTE)) {
+            try {
+                setPosicion(new Posicion(posicion.getFila()+1,posicion.getColumna()));
+                totalMovimientos++;
+            }catch (IllegalArgumentException e) {
+                throw new OperationNotSupportedException("ERROR");
+            }
+        } else if (d.equals(Direccion.ESTE)) {
+            try {
+                int numerocolumna= posicion.getColumna()+1;
+                setPosicion(new Posicion(posicion.getFila(),(char)numerocolumna));
+                totalMovimientos++;
+            }catch (IllegalArgumentException e) {
+                throw new OperationNotSupportedException("ERROR");
+            }
+        } else if (d.equals(Direccion.OESTE)) {
+            try {
+                int numerocolumna= posicion.getColumna()-1;
+                setPosicion(new Posicion(posicion.getFila(),(char)numerocolumna));
+                totalMovimientos++;
+            }catch (IllegalArgumentException e) {
+                throw new OperationNotSupportedException("ERROR");
+            }
+        } else if (d.equals(Direccion.NORESTE)) {
+            try {
+                int numerocolumna= posicion.getColumna()+1;
+                setPosicion(new Posicion(posicion.getFila()+1,(char)numerocolumna));
+                totalMovimientos++;
+            }catch (IllegalArgumentException e) {
+                throw new OperationNotSupportedException("ERROR");
+            }
+        } else if (d.equals(Direccion.NOROESTE)) {
+            try {
+                int numerocolumna= posicion.getColumna()-1;
+                setPosicion(new Posicion(posicion.getFila()+1,(char)numerocolumna));
+                totalMovimientos++;
+            }catch (IllegalArgumentException e) {
+                throw new OperationNotSupportedException("ERROR");
+            }
+        } else if (d.equals(Direccion.SURESTE)) {
+            try {
+                int numerocolumna= posicion.getColumna()+1;
+                setPosicion(new Posicion(posicion.getFila()-1,(char)numerocolumna));
+                totalMovimientos++;
+            }catch (IllegalArgumentException e) {
+                throw new OperationNotSupportedException("ERROR");
+            }
+        } else if (d.equals(Direccion.SUROESTE)) {
+            try {
+                int numerocolumna= posicion.getColumna()-1;
+                setPosicion(new Posicion(posicion.getFila()-1,(char)numerocolumna));
+                totalMovimientos++;
+            }catch (IllegalArgumentException e) {
+                throw new OperationNotSupportedException("ERROR");
+            }
+        } else if (d.equals(Direccion.ENROQUE_CORTO)) {
+            if(totalMovimientos>0){
+                throw new IllegalArgumentException("ERROR no es posible hacer enroque Rey tiene movimientos");
+            }else{
+                int numerocolumna= posicion.getColumna()+2;
+                setPosicion(new Posicion(posicion.getFila(), (char)numerocolumna));
+            }
+        } else if (d.equals(Direccion.ENROQUE_LARGO)) {
+            if(totalMovimientos>0){
+                throw new IllegalArgumentException("ERROR no es posible hacer enroque Rey tiene movimientos");
+            }else {
+                int numerocolumna= posicion.getColumna()-2;
+                setPosicion(new Posicion(posicion.getFila(), (char)numerocolumna));
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Rey{" +
+                "color=" + color +
+                ", posicion=" + posicion +
+                '}';
     }
 }
