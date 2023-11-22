da3b8bbd2dd0a87f59bb6376336308f6a112cfc8
Acapeltorres <luchocapel@icloud.com>
2023-11-22 18:10:17 +0100

    Añado metodo mostrarMenu a la clase Consola

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Consola.java
@@ -1,8 +1,21 @@
 package org.iesalandalus.programacion.reyajedrez;
 
+import java.sql.SQLOutput;
+
 public class Consola {
 
     private Consola(){
+
+    }
+
+
+   public static void mostrarMenu(){
+
+       System.out.println("Elige la opcion que desees realizar" +
+               "\n 1.Crear Rey por defecto." +
+               "\n 2.Crear Rey eligiendo color." +
+               "\n 3. Mover." +
+               "\n 4. Salir");
 
     }
 }
